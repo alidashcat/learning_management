ee8fdb1f4e9ec2ea2b4a807c36421b918c78f124
alidashcat <91668079+alidashcat@users.noreply.github.com>
2024-06-25 10:33:24 +0100

    create new app and authenticate admin

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations]
+  devise_for :users
+  resources :courses
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "admin" => "admin#index"
+
+  # Defines the root path route ("/")
+  root "courses#index"
+end
